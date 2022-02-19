d9a6c687647e184822cddca28b9e09e59b45c37b
BuXeMond <61242682+BuXeVi1@users.noreply.github.com>
2022-02-19 22:55:11 +0700

    commit

---
+++ Age/age.py
@@ -0,0 +1,17 @@
+#input
+age=int(input("ป้อนอายุของคุณ :"))
+
+#procress & output
+if age>=30 and age <=39:
+    print("วัยผู้ใหญ่")
+elif age>=20 and age <=29:
+    print("วัยหนุ่มสาว")
+elif age>=15 and age <=20:
+    print("วัยรุ่น")
+elif age >= 80:
+    print("วัยชรา")
+else :
+    print("วัยเด็ก")
+
+#output
+print("จบการทำงาน")
